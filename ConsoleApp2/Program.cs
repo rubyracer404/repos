9a7e14fc7efebf92691a480b4abde62f98c216d6
opilane <127192852+32henri@users.noreply.github.com>
2024-02-16 14:09:00 +0200

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            var domains = new Dictionary<string, string>()
+            {
+                {"est","Estonia"},
+                {"lat","Latvia"},
+                {"ba","Bgiosnia and herzegovina"},
+                {"Rus","Russia"}
+
+            };
+            int i = 1;
+            foreach (var domain in domains)
+            {
+                {
+                    i++;
+                    Console.WriteLine($"{domain.Key} - {domain.Value}");
+                }
+            }
+        }
+    }
+}
